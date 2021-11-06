0b7c313b728aaaa601732dbe50607a14fb90472a
Jasmeet <js3393187@gmail.com>
2021-11-06 13:38:04 +0530

    Performance improvements

--- commonChro.py
+++ commonChro.py
@@ -2,84 +2,35 @@ import matplotlib.pyplot as plt
 import matplotlib as mat
 import numpy as np
 import pandas as pd
-from openpyxl import load_workbook
 from collections import defaultdict
 
-#Sorting the excel files by Begin Location and rewriting the original one
-def SortData(path1p, path2p, ascending_p = True):
-    print('Sorting data...this might take some time')
-    df1 = pd.read_excel(r"{0}".format(path1p))
-    df2 = pd.read_excel(r"{0}".format(path2p))
+#Sorting the excel file by Begin Location
+def SortData(dataFrame_p, ascending_p = True):
+    print('Sorting data...')
 
-    sorted1 = df1.sort_values(by = 'Begin Location', ascending = ascending_p)
-    sorted2 = df2.sort_values(by = 'Begin Location', ascending = ascending_p)
-
-    #dataframe = pd.DataFrame(sorted1)
-    sorted1.to_excel(r"{0}".format(path1p), index = False)
-    #dataframe = pd.DataFrame(sorted2)
-    sorted2.to_excel(r"{0}".format(path2p), index = False)
-    print('Done.\n')
-
-#Storing data in 2 dictionaries and returning them
-def storeData(sheetName1, sheetName2):
-
-    #creating dictonaries to store the location and their respective chromosomes
-    chromlist1 = defaultdict(list)
-    chromlist2 = defaultdict(list)
-
-    #To store the column number of location and chromosomes in the excel file
-    colName1 = {}
-    colName2 = {}
-    colNum = 0
+    dataFrame_p.sort_values(by = 'Begin Location', ascending = ascending_p, inplace = True)
     
-    #displaying max no. of rows
-    print(f"Total number of rows/entries in {display_path1[:-5]}: {sheetName1.max_row}")
-    print(f"Total number of rows/entries in {display_path2[:-5]}: {sheetName2.max_row}")
+    print('Done.\n')
+    return dataFrame_p
+
+#Storing data in a dictionary and returning it
+def storeData(file_dict):
+    #creating dictonary to store the location and their respective chromosomes
+    chromlist = defaultdict(list)
+
     print("Processing...")
     
-    #Finding the chromosome and location columns in first file
-    for cols1 in sheetName1.iter_rows(min_row = 1, max_row = 1, min_col = 1, max_col = sheetName1.max_column, values_only = True):
-        for i in range(sheetName1.max_column):
-            if (cols1[i].lower() == "chromosome"):
-                colName1[cols1[i].lower()] = colNum
-            elif (cols1[i].lower() == "begin location"):
-                colName2[cols1[i].lower()] = colNum
-            
-            colNum += 1
-    
-    #Storing the chromosome and location from first excel file in chromlist1
-    for row1 in sheetName1.iter_rows(min_row = 2, min_col = colName1['chromosome'] + 1, max_col = colName2['begin location'] + 1, values_only = True):
-        if (row1[0] != None and row1[colName2['begin location'] - colName1['chromosome']] != None):
-            chromlist1[int(str(row1[colName2['begin location'] - colName1['chromosome']]))].append(row1[0])
+    #store the location and chromosomes in the excel file
+    file_loc = file_dict['Begin Location']
+    file_chro = file_dict['Chromosome']
 
-    #clearing the the lists and ressetting the colNum to 0 to store values for second file
-    colName1.clear()
-    colName2.clear()
-    colNum = 0
+    for loc, chro in zip(file_loc, file_chro):
+        chromlist[loc].append(chro)
 
-    #Finding the chromosome and begin location columns in second file
-    for cols2 in sheetName2.iter_rows(min_row = 1, max_row = 1, min_col = 1, max_col = sheetName2.max_column, values_only = True):
-        for i in range(sheetName2.max_column):
-            if (cols2[i].lower() == "chromosome"):
-                colName1[cols2[i].lower()] = colNum
-            elif (cols2[i].lower() == "begin location"):
-                colName2[cols2[i].lower()] = colNum
-            
-            colNum += 1
-
-    #Storing the chromosome and location from second excel file in chromlist2
-    for row2 in sheetName2.iter_rows(min_row = 2, min_col = colName1['chromosome'] + 1, max_col = colName2['begin location'] + 1, values_only = True):
-        if (row2[0] != None and row2[colName2['begin location'] - colName1['chromosome']] != None):
-            chromlist2[int(str(row2[colName2['begin location'] - colName1['chromosome']]))].append(row2[0])
-
-    #closing the excel files
-    workbook1.close()
-    workbook2.close()
-
-    return chromlist1, chromlist2
+    return chromlist
 
 #Finding common location and their respective chromosome
-def findCommon(chromList1, chromList2, loc = [], chro = {}):
+def findCommon(chromList1, chromList2, chro = {}):
     print("Finding common...")
 
     #Function to check and return which dataSet has more entries/values
@@ -99,7 +50,7 @@ def findCommon(chromList1, chromList2, loc = [], chro = {}):
     return chro
 
 #Plotting the values returned by the findCommon function
-def GraphPlot(commonChro_p, file_name1, file_name2):
+def PlotGraph(commonChro_p, file_name1, file_name2):
     #Data:
     chro_label = ['chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7',
             'chr8', 'chr9', 'chr10', 'chr11', 'chr12', 'chr13', 'chr14',
@@ -126,10 +77,9 @@ def GraphPlot(commonChro_p, file_name1, file_name2):
 
     #creating the bar plot
     fig, ax = plt.subplots(figsize = (16,9))
-    plt.tight_layout(pad = 5)
 
     #setting the x axis limits
-    ax.set_xlim([50, 31000000])
+    ax.set_xlim([50, 2_050_084_860])
 
     #creating an array of the number of labels present
     y = np.arange(len(chro_label))
@@ -141,7 +91,7 @@ def GraphPlot(commonChro_p, file_name1, file_name2):
             label = file_name2)
 
     #setting limits for x-axis ticks and their rotation
-    plt.xticks(np.arange(min(loc), 31000000, 900_000), rotation = 75)
+    plt.xticks(np.arange(min(loc), 2_050_084_860, 50_000_000), rotation = 75)
 
     #formatting how the values are displayed
     ax.xaxis.set_major_formatter(mat.ticker.StrMethodFormatter('{x: .0f}'))
@@ -187,25 +137,33 @@ def display():
 path1 = input("Enter path for file 1: ")
 path2 = input("Enter path for file 2: ")
 
-sorted_ask = input("Is the data/values to compare sorted?(Y/n) ")
+print("Reading Data...")
 
-if (sorted_ask.lower() != 'y'):
-    SortData(path1,path2)
+colNames_toread = ['Chromosome', 'Begin Location']
+df1 = pd.read_excel(r"{0}".format(path1))[colNames_toread]
+df2 = pd.read_excel(r"{0}".format(path2))[colNames_toread]
 
 display_path1 = path1.split('\\')[-1]
 display_path2 = path2.split('\\')[-1]
 
-#Loading the excel files in read only mode
-workbook1 = load_workbook(filename = r"{0}".format(path1), read_only = True)
-workbook2 = load_workbook(filename = r"{0}".format(path2), read_only = True)
+sorted_ask = input("Do you want to sort the data/values to compare ?(Y/n) ")
 
-#Loading the active sheet in excel file
-sheet1 = workbook1.active
-sheet2 = workbook2.active
+if (sorted_ask.lower() == 'y'):
+    df1 = SortData(df1)
+    df2 = SortData(df2)
 
-chromoList1, chromoList2 = storeData(sheet1, sheet2)
+#displaying max no. of rows
+print(f"Total number of rows/entries in {display_path1[:-5]}: {df1.shape[0]}")
+print(f"Total number of rows/entries in {display_path2[:-5]}: {df2.shape[0]}")
+
+df_dict_1 = df1.to_dict('list')
+df_dict_2 = df2.to_dict('list')
+
+chromoList1 = storeData(df_dict_1)
+chromoList2 = storeData(df_dict_2)
+
 commonChro = findCommon(chromoList1, chromoList2)
 
 display()
 
-GraphPlot(commonChro, display_path1[:-5], display_path2[:-5])
+PlotGraph(commonChro, display_path1[:-5], display_path2[:-5])
